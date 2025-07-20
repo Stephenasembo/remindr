324e571a56a03727bfb6fbc0fcbd40a31e079ef6
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-07-20 14:13:42 +0300

    Feat: Save Registered Users To The Database
    The users get saved and prisma errors are handled gracefully

---
+++ services/user.js
@@ -0,0 +1,42 @@
+const CustomError = require('../utils/customError');
+
+require('dotenv').config();
+const { PrismaClient, Prisma } = require('../generated/prisma');
+
+const databaseUrl =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
+const client = new PrismaClient({
+  datasources: {
+    db: {
+      url: databaseUrl,
+    },
+  },
+});
+
+async function createUser({ username, email, password }) {
+  try {
+    const user = await client.user.create({
+      data: { username, email, password },
+    });
+    return user;
+  } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      if (err.code === 'P2002') {
+        const constraint = err.meta.target[0].startsWith('username')
+          ? 'Username'
+          : 'Email';
+
+        const message = `${constraint} has to be unique.`;
+        throw new CustomError(400, 'Invalid Input', message);
+      }
+    }
+    throw new CustomError(500, 'Server Error', 'An internal error occured.');
+  }
+}
+
+module.exports = {
+  createUser,
+};
