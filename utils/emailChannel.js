ed03e51d3f4455a46950b66c987537a27d1bde5f
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-08-04 18:56:47 +0300

    Feat: Delete Non Recurring Reminders
    After a reminder is sent it is deleted if it does not recurr

--- utils/emailChannel.js
+++ utils/emailChannel.js
@@ -1,7 +1,11 @@
-const { updateStatus } = require('../services/reminder');
+const { updateStatus, deleteReminder } = require('../services/reminder');
 
 async function sendEmail(reminder) {
   console.log('email sent');
+  if (!reminder.recurring) {
+    await deleteReminder(reminder.id);
+    return;
+  }
   await updateStatus(reminder.id, 'SENT');
 }
 
